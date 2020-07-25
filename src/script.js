acb6f29a475f98dffb1a19bfc1bc9263a71f36a1
Ashish S <ashish.cgwcn@gmail.com>
2020-07-25 20:59:19 +0530

    background color gradient

--- src/script.js
+++ src/script.js
@@ -3,10 +3,22 @@ var app = new Vue({
   data: {
     bgType: "Color",
     bgColor: "#000000",
+    bgGradient1: "#ff0000",
+    bgGradient2: "#0000ff",
+    bgGradientDeg: "45deg",
   },
   computed: {
     artboardStyle: function () {
+      if (this.bgType === "Color") return this.artboardBgColor;
+      else return this.artboardBgGradient;
+    },
+    artboardBgColor: function () {
       return { backgroundColor: this.bgColor };
+    },
+    artboardBgGradient: function () {
+      return {
+        backgroundImage: `linear-gradient(${this.bgGradientDeg},${this.bgGradient1}, ${this.bgGradient2})`,
+      };
     },
   },
 
